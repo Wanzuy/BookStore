0cfe3b7e4c16335ba81c9fa585cb5fbe6dae000d
unknown <qn247732@gmail.com>
2024-09-25 23:35:20 +0700

    release-v1.0.1

--- mern-client/src/pages/home/About.jsx
+++ mern-client/src/pages/home/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-    return <div>NGO van truuuuam đã fix chuc nang about</div>;
+    return <div>NGO van truuuuam đã fix chuc nang about cho NDQ</div>;
 };
 
 export default About;
